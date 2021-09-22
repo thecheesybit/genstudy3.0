2c4b3f1268472aa8fe2dd8fcef17c95175c711a5
Nilesh Verma <nileshverma19@gmail.com>
2021-09-22 17:51:26 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
+  mongoURI: 'mongodb+srv://newton:Newton@009@cluster0.7pg8p.mongodb.net/Genstudy2.0?retryWrites=true&w=majority',
+  secretOrKey: "secret"
+};
